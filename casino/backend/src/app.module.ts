6184110c5799b70ef4e28580597a8a73f8f44ece
Nardos-Amakele <150350627+Nardos-Amakele@users.noreply.github.com>
2024-01-04 23:39:59 +0300

    new edits

---
+++ casino/backend/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import AuthModule from './auth/auth.module';
+import { DatabaseModule } from './db/database.module';
+
+@Module({
+  imports: [AuthModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+
+})
+export class AppModule {}
