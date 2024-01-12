6d50767fd49e23dfba15d123b0de2433a405d5a4
Teklez <zemenumekiria@gmail.com>
2024-01-12 20:00:00 +0300

    draft

--- casino/frontend/src/scripts/login-register.ts
+++ casino/frontend/src/scripts/login-register.ts
@@ -1,4 +1,153 @@
-// form validation
+// Bckend API endpoints
+
+const API_URL = "http://localhost:5000/";
+const LOGIN_URL = "http://localhost:5000/auth/login";
+const REGISTER_URL = "http://localhost:5000/auth/signup";
+const DELETE_ACCOUNT_URL = "http://localhost:5000/auth/delete-account";
+const BLOCK_USER_URL = "http://localhost:5000/auth/block";
+// form validation validation for registration
+
+function validateRegisterForm() {
+  const userName = (<HTMLInputElement>(
+    document.getElementById("register-userName")
+  )).value.trim();
+  const email = (<HTMLInputElement>(
+    document.getElementById("email")
+  )).value.trim();
+  const password = (<HTMLInputElement>(
+    document.getElementById("register-password")
+  )).value.trim();
+  const confirmPassword = (<HTMLInputElement>(
+    document.getElementById("confirmPassword")
+  )).value.trim();
+
+  resetRegistrationErrorMessages();
+
+  if (!userName) {
+    displayRegistrationErrorMessage("User Name is required.");
+    return;
+  }
+
+  if (/\d/.test(userName)) {
+    displayRegistrationErrorMessage("User Name should not contain numbers.");
+    return;
+  }
+
+  if (userName.length < 3) {
+    displayRegistrationErrorMessage(
+      "User Name must be at least 3 characters long."
+    );
+    return;
+  }
+
+  if (!email) {
+    displayRegistrationErrorMessage("Email is required.");
+    return;
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    displayRegistrationErrorMessage("Invalid email format.");
+    return;
+  }
+
+  if (!password) {
+    displayRegistrationErrorMessage("Password is required.");
+    return;
+  }
+
+  if (password.length < 8) {
+    displayRegistrationErrorMessage(
+      "Password must be at least 8 characters long."
+    );
+    return;
+  }
+
+  if (!/\d/.test(password) || !/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+    displayRegistrationErrorMessage(
+      "Password must contain at least one number and one symbol."
+    );
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    displayRegistrationErrorMessage(
+      "Passwords do not match. Please try again."
+    );
+    return;
+  }
+
+  // displayRegistrationSuccessMessage("Registration successful!");
+}
+
+function resetRegistrationErrorMessages() {
+  var errorMessagesElement = document.getElementById("errorMessages");
+  errorMessagesElement.textContent = "";
+  errorMessagesElement.style.color = "";
+}
+
+function displayRegistrationErrorMessage(message: string) {
+  var errorMessagesElement = document.getElementById("errorMessages");
+  errorMessagesElement.textContent = message;
+  errorMessagesElement.style.color = "red";
+}
+
+// function displayRegistrationSuccessMessage(message: string) {
+//   var errorMessagesElement = document.getElementById("errorMessages");
+//   errorMessagesElement.textContent = message;
+//   errorMessagesElement.style.color = "green";
+// }
+
+function validateLoginForm() {
+  const userName = (<HTMLInputElement>(
+    document.getElementById("login-userName")
+  )).value.trim();
+  const password = (<HTMLInputElement>(
+    document.getElementById("login-password")
+  )).value.trim();
+
+  resetErrorMessages();
+
+  if (!userName) {
+    displayErrorMessage("User Name is required.");
+    return;
+  }
+
+  if (/\d/.test(userName)) {
+    displayErrorMessage("User Name should not contain numbers.");
+    return;
+  }
+
+  if (userName.length < 3) {
+    displayErrorMessage("User Name must be at least 3 characters long.");
+    return;
+  }
+  if (!password) {
+    displayErrorMessage("Password is required.");
+    return;
+  }
+  resetErrorMessages();
+
+  // displaySuccessMessage("Login successful!");
+}
+
+function resetErrorMessages() {
+  var errorMessagesElement = document.getElementById("eerrorMessages");
+  errorMessagesElement.textContent = "";
+  errorMessagesElement.style.color = "";
+}
+
+function displayErrorMessage(message: string) {
+  var errorMessagesElement = document.getElementById("eerrorMessages");
+  errorMessagesElement.textContent = message;
+  errorMessagesElement.style.color = "red";
+}
+
+// function displaySuccessMessage(message: string) {
+//   var errorMessagesElement = document.getElementById("eerrorMessages");
+//   errorMessagesElement.textContent = message;
+//   errorMessagesElement.style.color = "green";
+// }
 
 //FORMS
 const loginForm: HTMLElement | null = document.getElementById("login-form");
@@ -9,6 +158,7 @@ const registerForm: HTMLElement | null =
 
 loginForm?.addEventListener("submit", (e) => {
   e.preventDefault();
+  validateLoginForm();
   const username = (<HTMLInputElement>document.getElementById("login-userName"))
     .value;
   const password = (<HTMLInputElement>document.getElementById("login-password"))
@@ -19,21 +169,6 @@ loginForm?.addEventListener("submit", (e) => {
   //call login function
   loginUser(username, password);
 });
-
-// if (registerForm){
-//     registerForm.addEventListener("submit", (e) => {
-//       e.preventDefault();
-//       registerUser(username, password);
-//     });
-// }
-
-// Bckend API endpoints
-
-const API_URL = "http://localhost:5000/";
-const LOGIN_URL = "http://localhost:5000/auth/login";
-const REGISTER_URL = "http://localhost:5000/auth/signup";
-
-const DELETE_ACCOUNT_URL = "http://localhost:5000/auth/delete-account";
 
 // Login
 
@@ -56,8 +191,11 @@ async function loginUser(username: string, password: string): Promise<any> {
       console.log("and the access token from the  cookie is :");
       console.log(getCookie(username));
       console.log("Login successful:", data);
+      (<HTMLInputElement>document.getElementById("login-userName")).value = "";
+      (<HTMLInputElement>document.getElementById("login-password")).value = "";
     } else {
       console.log("Login failed:", response.json());
+      alert("Username or password is incorrect");
     }
   } catch (error) {
     console.log("Login failed:", error);
@@ -94,6 +232,7 @@ function getCookie(name: string): string | null {
 
 registerForm?.addEventListener("submit", (e) => {
   e.preventDefault();
+  validateRegisterForm();
   const username = (<HTMLInputElement>(
     document.getElementById("register-userName")
   )).value;
@@ -135,6 +274,12 @@ async function registerUser(
     if (response.ok) {
       const data = await response.json();
       console.log("Registeration successful:", data);
+      (<HTMLInputElement>document.getElementById("register-userName")).value =
+        "";
+      (<HTMLInputElement>document.getElementById("email")).value = "";
+      (<HTMLInputElement>document.getElementById("register-password")).value =
+        "";
+      (<HTMLInputElement>document.getElementById("confirmPassword")).value = "";
     } else {
       const error = await response.json();
       console.log("Registeration failed", error);
